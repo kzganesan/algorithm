8c8c54e00a8d6b38dfa4c2336e4ad56fdaf30e41
Karthikeyan Ganesan <>
2021-01-26 21:23:03 -0600

    initial

--- src/main/java/org/example/LongestPalindrome.java
+++ src/main/java/org/example/LongestPalindrome.java
@@ -3,8 +3,39 @@ package org.example;
 public class LongestPalindrome {
     public static String longestPalindrome(String s){
         String rtn = "";
+        String findAns1 = "";
+        String findAns2 = "";
+        int count = 0;
 
+        for(int i=0; i< s.length(); i++){
+            findAns1 = palindrome(i, i, s);
+            findAns2 = palindrome(i-1, i, s);
+            System.out.println(findAns2);
+
+            if(Math.max(findAns1.length(), findAns2.length()) > count){
+                if(findAns1.length() > findAns2.length()){
+                    count = findAns1.length();
+                    rtn = findAns1;
+                }else{
+                    count = findAns2.length();
+                    rtn = findAns2;
+                }
+            }
+        }
 
         return rtn;
+    }
+
+    public static String palindrome(int i, int j, String s){
+        String rtn = "";
+
+        while(i>=0 && j< s.length() && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
+        }
+        i++;
+        System.out.println(i);
+        System.out.println(j);
+        return s.substring(i,j);
     }
 }
--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -7,9 +7,13 @@ public class MainClass {
 
         int[] num1 = new int[]{1,2};
         int[] num2 = new int[]{3,4};
+
         //System.out.println(FindMedianSortedArrays.findMedianSortedList(num1, num2));
 
-        System.out.println(ReverseInt.reverse(112));
+        //System.out.println(ReverseInt.reverse(112));
+
+        System.out.println("aa".substring(1,2));
+        System.out.println(LongestPalindrome.longestPalindrome("aa"));
     }
 
 
